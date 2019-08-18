c17c3439b1da4c6cc06458ae3caef95e78fa8062
lixiaoya0313 <54232053+lixiaoya0313@users.noreply.github.com>
2019-08-18 20:55:08 +0800

    Update execise_001.py

--- execise_001.py
+++ execise_001.py
@@ -1,2 +1,14 @@
+#
+# Q1: give a number, 
+# if is bigger than 0 print "POSITIVE", 
+# if it is smaller than 0 print "NEGATIVE", 
+# if it is equal to 0 print "ZERO".
+# 
 
-print("hello, welcome to python")
+num = 10
+if num > 0:
+    print  ('POSITIVE')
+elif num < 0:
+    print ('NEGATIVE')
+else:
+    num = ('ZERO')
